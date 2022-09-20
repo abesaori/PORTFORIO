42e88fd30f054ae856676d50ed6a3a876acddd14
abesaori <bebe.uranus@gmail.com>
2022-09-21 08:02:30 +0900

    scloll

--- script.js
+++ script.js
@@ -62,7 +62,33 @@ $('#wave').wavify({
     color: '#0bd',
     speed: .25
   });
-  //   フォーム
+
+  //// to top///
+  $(function(){
+    // TOPに戻るボタン
+        var topBtn = $('#page-top');
+        topBtn.hide();
+     
+        //スクロールが100に達したらボタン表示
+        $(window).scroll(function () {
+            var w = $(window).width();
+            if ($(this).scrollTop() > 100) {
+                topBtn.fadeIn();
+            } else {
+                topBtn.fadeOut();
+            }
+        });
+     
+        //スムーススクロールでページトップへ
+        topBtn.click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+    });
+    
+    //   フォーム
 jQuery("#my-form").submit(function (event) {
 	event.preventDefault();
 
